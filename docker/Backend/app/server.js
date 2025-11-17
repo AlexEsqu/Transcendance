3a7d04f82426c2f7dc12e4c20b79e3e5a35e3898
Elvire <lasablon@student.42.fr>
2025-11-17 20:37:04 +0100

    added some dependencies, unfinished...

--- docker/Backend/app/server.js
+++ docker/Backend/app/server.js
@@ -1,15 +1,18 @@
-const fastify = require('fastify')();
-
-const { ADDRESS = 'localhost', PORT = '3000' } = process.env;
-
-fastify.get('/', async (request, reply) => {
-  return { message: 'Hello world!' }
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
 })
 
-fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
 })
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
