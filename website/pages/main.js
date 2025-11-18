d3bc0e5adece2fe1c8c185d5c73b2e90c042a321
litchi <121684690+litchicoding@users.noreply.github.com>
2025-11-18 18:00:07 +0100

    Goal-Test : set objects in a scene and start animation

--- website/pages/main.js
+++ website/pages/main.js
@@ -1,30 +1,75 @@
 export const createScene = function () {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+const createScene = function () {
+	var scene = new BABYLON.Scene(engine);
+	scene.createDefaultLight();
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+	// Set the camera
+	var camera = new BABYLON.ArcRotateCamera(
+		'arCamera',
+		0, // alpha
+		1, // beta
+		10, // radius
+		new BABYLON.Vector3(0, 0, 0), // target
+		scene
+	);
+	// Allow to move the camera
+	camera.attachControl(canvas, true);
 
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+	// Set the light
+	var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+	light.intensity = -0.2;
+	var ball = BABYLON.MeshBuilder.CreateSphere(
+		'ball',
+		{
+			diameter: 0.5,
+			segments: 32
+		},
+		scene
+	);
+	ball.position.y = 0.3;
+	ball.position.z = 0;
+	ball.position.x = 0;
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+	// Set a the ground
+	var ground = BABYLON.MeshBuilder.CreateGround(
+		'ground',
+		{ 
+			width: 4,
+			height: 6 
+		}, 
+		scene
+	);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+	// Set a gizmo for debug
+	const gizmoManager = new BABYLON.GizmoManager(scene);
+	gizmoManager.positionGizmoEnabled = true;
+	gizmoManager.attachToMesh(ball);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+	// Code below provokes a crash
+	// while (ball.position.z != 3)
+	// 	ball.position.z += 0.01;
+	// while (ball.position.z != -3)
+	// 	ball.position.z -= 0.01;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+	// Try to animate the ball
+	// Try to animate the ball
+	const ballAnimation = new BABYLON.Animation(
+		'ballAnimation',
+		'position.z',
+		30,
+		BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+		BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+	);
+	const animationKeys = [];
+	animationKeys.push({frame: 0, value: -3});
+	animationKeys.push({frame: 30, value: 0});
+	animationKeys.push({frame: 60, value: 3});
+	animationKeys.push({frame: 90, value: 0});
+	animationKeys.push({frame: 120, value: -3});
+	ballAnimation.setKeys(animationKeys);
+	ball.animations = [];
+	ball.animations.push(ballAnimation);
+	scene.beginAnimation(ball, 0, 120, true);
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
-
-    return scene;
+	return scene;
 };
