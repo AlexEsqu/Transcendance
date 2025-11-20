623990b177748356dcfae98146693decca15de7e
Alex <momiji.kling@gmail.com>
2025-11-20 14:16:40 +0100

    feat(reloading): add Dockerfile et Dockercompose, amend architecture to have website reload with any new code

--- Frontend/src/app.ts
+++ Frontend/src/app.ts
@@ -18,8 +18,8 @@ class App {
 
         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        var light1: HemisphericLight = new HemisphericLight("light2", new Vector3(1, 1, 0), scene);
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1, }, scene);
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
--- Frontend/tool/start.sh
+++ Frontend/tool/start.sh
@@ -3,8 +3,14 @@
 # super useful debug flag
 set -e
 
+# move to correct directory (not necessary if WORKDIR set)
+cd /app
+
 # build JS project using the JS package manager
 npm run build
 
-# replacing main process with npm run start
-exec npm run start
+# # replacing main process with npm run start
+# exec npm run start
+
+# for developpment: live reload
+npx webpack serve --config webpack.config.js --mode development --host 0.0.0.0 --port 8080 --hot
