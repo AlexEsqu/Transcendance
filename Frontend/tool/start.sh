96d9a8dabf1545309a789480ef5c9dfaf003a61d
Alex <momiji.kling@gmail.com>
2025-11-21 14:13:39 +0100

    feat(LiveServer): Exchanged Webpack for liveserver

--- Frontend/tool/start.sh
+++ Frontend/tool/start.sh
@@ -6,11 +6,13 @@ set -e
 # move to correct directory (not necessary if WORKDIR set)
 cd /app
 
+# install dependencies if not present (since .gitignore the heavy node-modules)
+if [ ! -d node_modules ]; then
+  npm install
+fi
+
 # build JS project using the JS package manager
 npm run build
 
-# # replacing main process with npm run start
-# exec npm run start
-
-# for developpment: live reload
-npx webpack serve --config webpack.config.js --mode development --host 0.0.0.0 --port 8080 --hot
+# replacing main process with npm run start
+exec npm run dev
