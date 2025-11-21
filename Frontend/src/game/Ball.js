5011d792d0d2f7e6dc6febfa9814c283bf034337
lucie vallee <vallee.lucie99@gmail.com>
2025-11-21 20:11:48 +0100

    Add: creation ball and paddle class, user input in progress

--- Frontend/src/game/Ball.js
+++ Frontend/src/game/Ball.js
@@ -3,15 +3,6 @@ import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import { Engine, Scene, ArcRotateCamera, MeshBuilder, Vector3, HemisphericLight } from '@babylonjs/core';
 
-export class Ball {
-	#lastPaddleHit;
-	#speed
-
-	constructor(sceneId) {
-
-	}
-}
-
 // Contains every data about the ball
 // Handle position, velocity, direction
 // Detects collision with walls and paddles
@@ -25,3 +16,47 @@ export class Ball {
 // function to update position of the ball according to delta time, collision etc
 // reset values function
 // collision detection function
+
+export class Ball {
+	constructor(scene) {
+		this.scene = scene;
+		this.mesh = MeshBuilder.CreateSphere('ball', { diameter: 0.5 }, scene);
+		this.mesh.position = new Vector3(0, 1, 0);
+	}
+
+	update() {
+		// const speed = 5;
+		// this.mesh.position.x += 0.1;
+		// if (this.mesh.position.x >= 6)
+		// 	this.mesh.position.x = 0;
+		// const targetPos = this.mesh.position.add(Vector3(1, 1, 1));
+		// check for collisions detection
+		// this.animate(targetPos, 2);
+	}
+
+	/**
+	 * Should have 2 options :
+	 * 	- launch from the last winner paddle
+	 * 	- launch in random direction from the middle of the ground
+	 */
+	launch() {
+
+	}
+
+	/**
+	 * Reset position of the ball
+	 */
+	reset() {
+
+	}
+
+	/**
+	 * There is 3 cases :
+	 * 	- new pos is out of bounds = last hit paddle wins
+	 * 	- new pos is in wall = invert direction according to velocity
+	 * 	- new pos is in padlle = update last paddle hit and new dir
+	 */
+	checkForCollisions() {
+
+	}
+}
