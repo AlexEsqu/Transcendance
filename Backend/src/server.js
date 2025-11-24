0ab5459f128c0386e0862ee4ad343650c74cdfce
Elvire <lasablon@student.42.fr>
2025-11-24 17:55:03 +0100

    app => server for more clarity

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,10 @@
 // ESM
 import Fastify from 'fastify'
 import fs from "fs";
-import root from './routes/root.js'
-const app = Fastify({
-  logger: true,
+import db from './database.js';
+import getUsers from './routes/users.js';
+
+export const server = Fastify({
   https: {
     key: fs.readFileSync("/tmp/certs/server.key"),
     cert: fs.readFileSync("/tmp/certs/server.crt")
@@ -11,19 +12,19 @@ const app = Fastify({
 });
 
 
-app.register(root);
+server.register(getUsers, { prefix: "/api" });
 
 /**
  * Run the server!
  */
 const start = async () => {
 	try {
-		await app.listen({
+		server.listen({
 			port: process.env.PORT,
 			host: process.env.ADDRESS ,
 		});
 	} catch (err) {
-		app.log.error(err);
+		server.log.error(err);
 		process.exit(1);
 	}
 };
