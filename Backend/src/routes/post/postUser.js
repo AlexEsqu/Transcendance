828efbc502a93f5785ea5a2161d3f1185e231d9e
Elvire <lasablon@student.42.fr>
2025-11-25 18:43:37 +0100

    added password hashing

--- Backend/src/routes/post/postUser.js
+++ Backend/src/routes/post/postUser.js
@@ -1,4 +1,6 @@
 import db from "/app/src/database.js";
+import bcrypt from "bcrypt";
+import { handleSQLiteError } from "../../errors/sqliteErrors.js";
 
 function postUser(server) {
 	const opts = {
@@ -12,20 +14,25 @@ function postUser(server) {
 				},
 			},
 		},
-		handler: function (req, res) {
-			try {
-				const { username, password } = req.body;
-				const addUser = db.prepare(`INSERT INTO users(username,password_hash) VALUES (?, ?)`);
-				addUser.run(username, password);
-
-				res.send({ message: "User received", username });
-			} catch (err) {
-				handleSQLiteError(err, reply);
-			}
+		handler: (request, reply) => {
+			const { username, password } = request.body;
+			let saltRounds = 10;
+			// Hash password using callback style
+			bcrypt.hash(password, saltRounds, (err, hash) => {
+				if (err) {
+					console.error("Bcrypt error:", err.message);
+					return reply.status(500).send({ error: "Password hashing failed" });
+				}
+				try {
+					const addUser = db.prepare(`INSERT INTO users(username, password_hash) VALUES (?, ?)`);
+					addUser.run(username, hash);
+					reply.send({ message: "User created", username });
+				} catch (dbErr) {
+					handleSQLiteError(dbErr, reply);
+				}
+			});
 		},
 	};
-
 	server.post("/users", opts);
 }
-
 export default postUser;
