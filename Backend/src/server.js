5cc80b028d620a93d16d203afc0c64db2cc1258b
Elvire <lasablon@student.42.fr>
2025-11-26 14:18:16 +0100

    added the auth plugin

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,20 +1,25 @@
 // ESM
-import Fastify from 'fastify'
+import Fastify from "fastify";
 import fs from "fs";
-import fastifyFormBody from '@fastify/formbody'
-import fastifyMultiPart from '@fastify/multipart'
-import {getUsers, getUser} from './routes/users/getUsers.js';
-import postUser from './routes/users/postUser.js';
+import fastifyFormBody from "@fastify/formbody";
+import fastifyMultiPart from "@fastify/multipart";
+import authPlugin from "plugins/jwt.js";
+import { getUsers, getUser } from "./routes/users/getUsers.js";
+import postUser from "./routes/users/userSignup.js";
 
 export const server = Fastify({
-  https: {
-    key: fs.readFileSync("/tmp/certs/server.key"),
-    cert: fs.readFileSync("/tmp/certs/server.crt")
-  }
+	https: {
+		key: fs.readFileSync("/tmp/certs/server.key"),
+		cert: fs.readFileSync("/tmp/certs/server.crt"),
+	},
 });
 
+// MODULES
 server.register(fastifyFormBody);
 server.register(fastifyMultiPart);
+server.register(authPlugin);
+
+//ROUTES
 server.register(getUsers, { prefix: "/api" });
 server.register(getUser, { prefix: "/api" });
 server.register(postUser, { prefix: "/api" });
@@ -26,7 +31,7 @@ const start = async () => {
 	try {
 		server.listen({
 			port: process.env.PORT,
-			host: process.env.ADDRESS ,
+			host: process.env.ADDRESS,
 		});
 	} catch (err) {
 		server.log.error(err);
