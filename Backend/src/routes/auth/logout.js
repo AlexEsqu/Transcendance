74b883ea025d890a6f3270d980318643fcfd76d7
Elvire <lasablon@student.42.fr>
2025-11-27 16:36:48 +0100

    Added logout route

--- Backend/src/routes/auth/logout.js
+++ Backend/src/routes/auth/logout.js
@@ -1,9 +1,32 @@
 import db from "/app/src/database.js";
 import bcrypt from "bcrypt";
 
-
 function logout(server) {
+	const opts = {
+		schema: {
+			tags: ["auth"],
+			description: "Will log out the user",
+			security: [{ BearerAuth: [], cookieAuth: [] }], // access token required
+		},
+		onRequest: [server.authenticate],
+	};
 	server.post("/auth/logout", opts, async (req, reply) => {
-		
-})
+		//Clear the refresh token from db
+		const { id, username } = req.user;
+		console.log(id);
+		db.prepare(`UPDATE users SET refresh_token_hash = NULL WHERE id = ?`).run(id);
+
+		//Clear the refresh token from cookies
+		reply.clearCookie("refreshToken", {
+			httpOnly: true,
+			secure: true,
+			sameSite: "strict",
+			path: "/",
+		});
+
+		// Send confirmation
+		return reply.send({ success: true, message: "Logged out " + username + " successfully" });
+	});
 }
+
+export default logout;
