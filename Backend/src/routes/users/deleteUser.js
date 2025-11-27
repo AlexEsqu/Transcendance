e67ad94da03fc16ee7ed5dde8bab2fa7bc7ba758
Elvire <lasablon@student.42.fr>
2025-11-27 19:16:48 +0100

    fixed the delete path

--- Backend/src/routes/users/deleteUser.js
+++ Backend/src/routes/users/deleteUser.js
@@ -7,22 +7,11 @@ function deleteUser(server) {
 			tags: ["user"],
 			description: "Deletes the user",
 			security: [{ BearerAuth: [] }],
-			params: {
-				type: "object",
-				properties: {
-					id: { type: "integer", minimum: 1 },
-				},
-				required: ["id"],
-			},
 		},
 		onRequest: [server.authenticate],
 	};
-	server.delete("/users/:id/delete", opts, (req, reply) => {
+	server.delete("/users/me", opts, (req, reply) => {
 		const { id } = req.user;
-		console.log(req.params.id);
-		if (id != req.params.id) 
-			return reply.status(401).send({ error: "Unauthorized" });
-
 		db.prepare(`DELETE FROM users WHERE id = ?`).run(id);
 		reply.status(204).send();
 	});
