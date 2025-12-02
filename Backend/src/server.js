1ad6a4d6c06fd45968399a00b1372bd7a9d9fc19
Elvire <lasablon@student.42.fr>
2025-12-02 17:32:30 +0100

    refactor

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,65 +1,5 @@
-// ESM
-import Fastify from "fastify";
-import fs from "fs";
-import fastifyFormBody from "@fastify/formbody";
-import fastifyMultiPart from "@fastify/multipart";
-import authPlugin from "./plugins/jwt.js";
-import swaggerPlugin from "./plugins/swagger.js";
-import fastifyCookie from "@fastify/cookie";
-import cors from "@fastify/cors";
+import { server } from "./app.js";
 
-import clientAuthPluggin from "./plugins/validateApiKey.js";
-import { getUsers, getUser } from "./routes/users/getUsers.js";
-import yaml from "yaml";
-import postUser from "./routes/users/signup.js";
-import login from "./routes/auth/login.js";
-import refresh from "./routes/auth/refresh.js";
-import logout from "./routes/auth/logout.js";
-import deleteUser from "./routes/users/deleteUser.js";
-import patchUserPassword from "./routes/users/patchUserPassword.js";
-import patchUserInfo from "./routes/users/patchUserInfo.js";
-import postMatches from "./routes/matches/postMatch.js";
-import getMatches from "./routes/matches/getMatches.js";
-import getUserMatches from "./routes/matches/getUserMatches.js";
-
-export const server = Fastify({
-	https: {
-		key: fs.readFileSync("/tmp/certs/server.key"),
-		cert: fs.readFileSync("/tmp/certs/server.crt"),
-	},
-});
-
-// MODULES
-server.register(fastifyFormBody);
-server.register(fastifyMultiPart);
-server.register(authPlugin);
-server.register(fastifyCookie);
-server.register(swaggerPlugin);
-server.register(clientAuthPluggin);
-// server.register(cors, {
-// 	origin: "*", // allow all origins;
-// 	methods: ["GET", "PATCH", "POST", "DELETE"],
-// });
-
-server.register(cors, { origin: '*', credentials: true, })
-
-//ROUTES
-server.register(getUsers);
-server.register(getUser);
-server.register(postUser, { prefix: "users" });
-server.register(login, { prefix: "users" });
-server.register(refresh, { prefix: "users" });
-server.register(logout, { prefix: "users" });
-server.register(deleteUser, { prefix: "users" });
-server.register(patchUserPassword, { prefix: "users" });
-server.register(patchUserInfo, { prefix: "users" });
-server.register(postMatches);
-server.register(getMatches);
-server.register(getUserMatches);
-
-/**
- * Run the server!
- */
 const start = async () => {
 	try {
 		server.listen({
@@ -72,6 +12,7 @@ const start = async () => {
 	}
 };
 start();
+
 // // write the api as yaml
 
 // server.ready((err) => {
