6a33f235051eee05fba5505bc2dafb029a15ea67
Alex <momiji.kling@gmail.com>
2025-12-02 13:46:07 +0100

    feat(login): add layout tweaks

--- Frontend/src/landing/alias.ts
+++ Frontend/src/landing/alias.ts
@@ -2,12 +2,40 @@ import { displayGamePage } from "../app";
 
 import welcomeHtml from "../pages/welcome.html";
 import guestinHtml from "../pages/guestin.html";
+import registerHtml from "../pages/register.html";
+import loginHtml from "../pages/login.html";
 
 
-export { displayAliasQueryPage, localStorageKeyForAlias }
+export { displayAliasQueryPage, removeGuestName }
 
 const localStorageKeyForAlias : string = "PongAlias"
 
+// replaces the document body with a menu page to choose to login, register or play as guest
+function displayAliasQueryPage() : void
+{
+	document.body.innerHTML = welcomeHtml;
+
+	const GuestInButton = document.getElementById('btn-guestin')
+	GuestInButton.addEventListener("click", function ()
+	{
+		console.log("clicking submit alias button");
+		displayGuestInPage();
+	})
+
+	const registerButton = document.getElementById('btn-register')
+	registerButton.addEventListener("click", function ()
+	{
+		console.log("clicking submit register button");
+		displayRegisterPage();
+	})
+}
+
+
+// removes guest name from localstorage, displays a query page
+function removeGuestName() : void
+{
+	localStorage.removeItem(localStorageKeyForAlias);
+}
 
 function displayGuestInPage() : void
 {
@@ -31,15 +59,32 @@ function displayGuestInPage() : void
 	})
 }
 
-function displayAliasQueryPage() : void
+function displayRegisterPage() : void
 {
-	document.body.innerHTML = welcomeHtml;
+	document.body.innerHTML = registerHtml
 
-	const GuestInButton = document.getElementById('btn-guestin')
-	GuestInButton.addEventListener("click", function ()
+	const loginInput : HTMLInputElement = document.getElementById('input-login') as HTMLInputElement;
+	const passwordInput : HTMLInputElement = document.getElementById('input-password') as HTMLInputElement;
+	const registerButton : HTMLElement = document.getElementById('btn-submit-register')
+
+	registerButton.addEventListener('click', function ()
 	{
-		console.log("clicking submit alias button");
-		displayGuestInPage();
+		// store contents of the inputs
+		const login = loginInput.value;
+		const password = passwordInput.value;
+
+		// reset input fields
+		loginInput.value = "";
+		passwordInput.value = "";
+
+		// log for debug (TO DO: remove)
+		console.log(`User tried to register with ${login} and ${password}`);
+
+		// checking the login is not an empty string (a profanity checker would be funny...)
+		if (login && false)
+		{
+
+			displayGamePage();
+		}
 	})
 }
-
--- Frontend/src/landing/nav.ts
+++ Frontend/src/landing/nav.ts
@@ -1,9 +1,10 @@
 import footerHTML from "../pages/footer.html"
 import headerHTML from "../pages/header.html"
-import { displayAliasQueryPage, localStorageKeyForAlias } from "./alias"
+import { displayAliasQueryPage, removeGuestName } from "./alias"
 
 export {displayFooter, displayHeader}
 
+// adds a footer to the document body
 function displayFooter() : void
 {
 	document.body.innerHTML += footerHTML
@@ -14,12 +15,18 @@ function displayFooter() : void
 	deleteAliasButton.addEventListener("click", function ()
 	{
 		console.log("clicking delete button");
-		localStorage.removeItem(localStorageKeyForAlias);
-		displayAliasQueryPage();
+		deleteUserData()
 	})
 }
 
+// adds a header to the document body, replace name into it
 function displayHeader(name : string) : void
 {
 	document.body.innerHTML += headerHTML.replace('WISELY', name)
+}
+
+function deleteUserData() : void
+{
+	removeGuestName();
+	displayAliasQueryPage();
 }
