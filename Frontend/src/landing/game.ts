4f9935244f1a519b2787f1d5423473ada6c0efec
Alex Kling <momiji.kling@gmail.com>
2025-12-02 09:32:57 +0100

    feat(tailwind): aspect ratio using tailwind woohoo

--- Frontend/src/landing/game.ts
+++ Frontend/src/landing/game.ts
@@ -2,19 +2,15 @@
 import { Pong } from "../game/Pong"
 
 import { createAttachElement } from "./utils";
+import gameHtml from '../pages/game.html'
 
 export { displayGame }
 
 class App {
-	constructor(container : HTMLElement) {
-		const canvas = document.createElement("canvas");
-		canvas.id = "gameCanvas";
-		canvas.width = 800;
-		canvas.height = 600;
-		container.appendChild(canvas);
+	constructor(canvas : HTMLElement) {
 		// const pong = new Pong("gameCanvas", "Popol", "Gaya");
 		requestAnimationFrame(() => {
-			const pong = new Pong("gameCanvas", undefined, undefined, true);
+			const pong = new Pong("game-canvas", undefined, undefined, true);
 			pong.loadGame();
 			pong.startPlay();
 		});
@@ -22,6 +18,9 @@ class App {
 }
 
 function displayGame() : void {
-	const gameWindow = createAttachElement("main", document.body, "game", "game");
+
+	document.body.innerHTML += gameHtml;
+
+	const gameWindow = document.getElementById("game-canvas");
 	new App(gameWindow);
 }
