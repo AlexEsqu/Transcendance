f681c2a3b4eafb5c5194ee4846751d93d90a74fb
Elvire <109591568+elvireauciel@users.noreply.github.com>
2025-12-22 18:02:26 +0100

    added conf

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
+	allowedHosts: [
+      "localhost",
+      "typescript",
+      "nginx"
+    ],
   },
   build: {
     outDir: 'dist',
