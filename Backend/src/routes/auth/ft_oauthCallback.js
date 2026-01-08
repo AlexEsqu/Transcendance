e7018be7cd7ebeef15cbc744a9274b71d0adb7e6
Elvire <109591568+elvireauciel@users.noreply.github.com>
2026-01-08 12:48:54 +0100

    added missing import

--- Backend/src/routes/auth/ft_oauthCallback.js
+++ Backend/src/routes/auth/ft_oauthCallback.js
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 import { generateTokens, sendVerificationCodeEmail } from "../../services/authServices.js";
-
+import fs from "fs"
 const redirectUrl = encodeURI(`${process.env.FRONTEND_DOMAIN_NAME}/settings`);
 
 export function ft_OAuth2_callback(server) {
