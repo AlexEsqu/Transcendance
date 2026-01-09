8bd9c8507d2b91787a984042ca01a026dcdecf15
Alex <momiji.kling@gmail.com>
2026-01-09 18:28:07 +0100

    fix: add visual front end form validation to connection pages

--- Frontend/src/auth/connectionPage.ts
+++ Frontend/src/auth/connectionPage.ts
@@ -1,5 +1,5 @@
 import { userState, router } from "../app";
-import { apiDomainName, apiKey } from "../user/UserState";
+import { isValidInputs, checkInputValidityOnUnfocus } from "../utils/inputValidation";
 
 import connectionHtml from "../html/connection.html?raw";
 import guestFormHtml from "../html/forms/guestForm.html?raw"
@@ -105,13 +105,20 @@ function onAliasLoaded(): void
 		mainContainer.insertAdjacentHTML('beforeend', guestFormHtml);
 
 	const guestForm = document.getElementById('guest-form') as HTMLFormElement | null;
+	if (!guestForm)
+		return
+
+	checkInputValidityOnUnfocus(guestForm);
 	guestForm?.addEventListener('submit', (e) =>
 		{
 			e.preventDefault();
 			const formData = new FormData(guestForm);
-			const alias = formData.get('input-alias') as string | null;
-			if (alias)
-				userState.guest.guestin(alias);
+			if (isValidInputs(guestForm.querySelectorAll('input')))
+			{
+				const alias = formData.get('input-alias') as string | null;
+				if (alias)
+					userState.guest.guestin(alias);
+			}
 		}
 	);
 }
@@ -123,34 +130,42 @@ function onRegisterLoaded(): void
 		mainContainer.insertAdjacentHTML('beforeend', registerFormHtml);
 
 	const registerForm = document.getElementById('register-form') as HTMLFormElement | null;
+	if (!registerForm)
+		return;
+
+	checkInputValidityOnUnfocus(registerForm);
 	registerForm?.addEventListener('submit', async (e) =>
 		{
 			e.preventDefault();
 			const formData = new FormData(registerForm);
-			const login = formData.get('input-login') as string | null;
-			const email = formData.get('input-email') as string | null;
-			const password = formData.get('input-password') as string | null;
-			const check = formData.get('input-password-check') as string | null;
 
-			if (!login || !password || !email)
-				return;
+			if (isValidInputs(registerForm.querySelectorAll('input')))
+			{
+				const login = formData.get('input-login') as string | null;
+				const email = formData.get('input-email') as string | null;
+				const password = formData.get('input-password') as string | null;
+				const check = formData.get('input-password-check') as string | null;
 
-			if (password !== check)
-			{
-				alert("The passwords don't match...");
-				return;
-			}
+				if (!login || !password || !email)
+					return;
 
-			try
-			{
-				await userState.emailAuth.register(login, password, email);
-				router.navigateTo("/connection/emailcheck");
-			}
-			catch (error)
-			{
-				const msg = error instanceof Error ? error.message : "Unknown error";
-				window.sessionStorage.setItem("errorMessage", msg);
-				router.navigateTo("/error");
+				if (password !== check)
+				{
+					alert("The passwords don't match...");
+					return;
+				}
+
+				try
+				{
+					await userState.emailAuth.register(login, password, email);
+					router.navigateTo("/connection/emailcheck");
+				}
+				catch (error)
+				{
+					const msg = error instanceof Error ? error.message : "Unknown error";
+					window.sessionStorage.setItem("errorMessage", msg);
+					router.navigateTo("/error");
+				}
 			}
 		}
 	);
@@ -163,26 +178,33 @@ function onLoginLoaded(): void
 		mainContainer.insertAdjacentHTML('beforeend', loginFormHtml);
 
 	const loginForm = document.getElementById('login-form') as HTMLFormElement | null;
-	loginForm?.addEventListener('submit', async (e) =>
+	if (!loginForm)
+		return;
+
+	checkInputValidityOnUnfocus(loginForm);
+	loginForm.addEventListener('submit', async (e) =>
 		{
 			e.preventDefault();
 
-			const formData = new FormData(loginForm);
-			const login = formData.get('input-login') as string | null;
-			const password = formData.get('input-password') as string | null;
-
-			if (login && password)
+			if (isValidInputs(loginForm.querySelectorAll('input')))
 			{
-				try
+				const formData = new FormData(loginForm);
+				const login = formData.get('input-login') as string | null;
+				const password = formData.get('input-password') as string | null;
+
+				if (login && password)
 				{
-					await userState.emailAuth.login(login, password);
-				}
-				catch (error)
-				{
-					const msg = error instanceof Error ? error.message : "Unknown error";
-					console.log(`error message is ${msg}`);
-					window.sessionStorage.setItem("errorMessage", msg);
-					router.navigateTo("/error");
+					try
+					{
+						await userState.emailAuth.login(login, password);
+					}
+					catch (error)
+					{
+						const msg = error instanceof Error ? error.message : "Unknown error";
+						console.log(`error message is ${msg}`);
+						window.sessionStorage.setItem("errorMessage", msg);
+						router.navigateTo("/error");
+					}
 				}
 			}
 		}
