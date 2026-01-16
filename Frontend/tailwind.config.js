78b17fef9cd33c201cae3abf77944387f311c3e1
Alex <momiji.kling@gmail.com>
2026-01-16 14:54:57 +0100

    fix: add responsive font size tailwind style

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
     "./src/html/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+		fontSize: {
+        'fluid-sm': 'clamp(0.875rem, 1.5vw, 1rem)',
+        'fluid-base': 'clamp(1rem, 2vw, 1.25rem)',
+        'fluid-lg': 'clamp(1.125rem, 2.5vw, 1.5rem)',
+        'fluid-xl': 'clamp(2rem, 4vw, 5rem)',
+      }
+	},
   },
   plugins: [],
 }
