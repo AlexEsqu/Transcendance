3b0e12b04a4f803d0a07cc6eb236082dad435c15
Alex Kling <m2kling@gmail.com>
2026-01-21 16:49:46 +0100

    feat(gamePageSub): GameApp subscribed to page change with boolean changing to false if page quit

--- Frontend/src/game/display.ts
+++ Frontend/src/game/display.ts
@@ -1,11 +1,6 @@
-import { userState, router } from "../app";
-import { launchPongGame } from "./GameApp"
-import { clearOptions, loadOptions, saveOptions } from "./options";
-import { IOptions } from "./pongData";
 import { GameOptionsModal } from "./GameOptionModal";
 
 import gameHtml from '../html/game.html?raw'
-import optionsHtml from '../html/forms/gameOptionsForm.html?raw'
 
 /************************************************************************************************************/
 
@@ -17,96 +12,6 @@ function getGamePage(): string
 {
 	return gameHtml;
 }
-
-// function initializeGameOptionsForm(root: HTMLElement): void
-// {
-// 	const macthTypeContainer = root.querySelector('match-type-container') as HTMLElement;
-// 	const locationSelect = root.querySelector('match-location') as HTMLSelectElement;
-
-// 	if (!macthTypeContainer)
-// 		throw new Error("'match-type-container' select not found");
-// 	else if (!locationSelect)
-// 		throw new Error("'match-location' select not found");
-
-// 	generateMatchType(macthTypeContainer, locationSelect, root);
-
-// 	const matchSelect = macthTypeContainer.querySelector('select') as HTMLSelectElement;
-
-// 	let nbOfPlayers: number = parseInt(matchSelect.value);
-// 	if (locationSelect.value === 'remote')
-// 		nbOfPlayers = 1;
-
-// 	generatePlayersInputs(nbOfPlayers, root);
-// 	generatePaddleColorsInputs(nbOfPlayers, root);
-
-// 	macthTypeContainer.addEventListener('change', function() {
-// 		const matchSelect = macthTypeContainer.querySelector('select') as HTMLSelectElement;
-
-// 		let newNbOfPlayers: number = parseInt(matchSelect.value);
-// 		if (locationSelect.value === 'remote')
-// 			newNbOfPlayers = 1;
-
-// 		generatePlayersInputs(newNbOfPlayers, root);
-// 		generatePaddleColorsInputs(newNbOfPlayers, root);
-// 	});
-
-// 	locationSelect.addEventListener('change', function() {
-// 		generateMatchType(macthTypeContainer, locationSelect, root);
-
-// 		const matchSelect = macthTypeContainer.querySelector('select') as HTMLSelectElement;
-
-// 		let newNbOfPlayers: number = parseInt(matchSelect.value);
-// 		if (locationSelect.value === 'remote')
-// 			newNbOfPlayers = 1;
-
-// 		generatePlayersInputs(newNbOfPlayers, root);
-// 		generatePaddleColorsInputs(newNbOfPlayers, root);
-// 	});
-// }
-
-// function getPlayerNames(root: HTMLElement): string[]
-// {
-// 	const playersContainer = root.querySelector('players-container');
-// 	if (!playersContainer)
-// 		throw new Error("No players found");
-
-// 	const inputs = playersContainer.querySelectorAll('input');
-// 	const result = Array.from(inputs).map(input => (input as HTMLInputElement).value || `Player ${input.id.replace('player', '')}`);
-// 	if ( result[0] && result[0] === 'Player 1')
-// 		result[0] = userState.getUser()?.getName() ?? 'Player 1';
-
-// 	return result;
-// }
-
-// function getPaddColors(root: HTMLElement): string[]
-// {
-// 	const paddColorsContainer = root.querySelector('paddle-colors-container');
-// 	if (!paddColorsContainer)
-// 		throw new Error("No players found");
-
-// 	userState.refreshUser();
-
-// 	const inputs = paddColorsContainer.querySelectorAll('input[type="color"]');
-// 	const result = Array.from(inputs).map(input => (input as HTMLInputElement).value || '#a2c2e8');
-
-// 	return result;
-// }
-
-// function getMatchType(root: HTMLElement): number
-// {
-// 	const matchContainer = root.querySelector('match-type-container');
-// 	if (!matchContainer)
-// 		throw new Error("No match type found");
-
-// 	userState.refreshUser();
-
-// 	const selection = matchContainer.querySelector('select');
-// 	if (!selection)
-// 		throw new Error("No selection found");
-
-// 	const result = parseInt(selection.value);
-// 	return (result);
-// }
 
 function setNotification(show: boolean, message: string | undefined): void
 {
@@ -136,5 +41,5 @@ function onGameLoaded(): void
 
 function cleanGamePage(): void
 {
-	// TO BE FILLED
+	GameApp.close
 }
